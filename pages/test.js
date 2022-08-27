b4bb190710772668b5255b87fe9b40f67700b741
Joël Vermeulen <98051242+JoelVerm@users.noreply.github.com>
2022-08-27 12:13:29 +0200

    fonts

--- pages/test.js
+++ pages/test.js
@@ -1,7 +1,8 @@
-import { navbar, navItem } from '../components/navbar.js'
+import { navbar, navItem, activePage } from '../components/navbar.js'
 
 flami(() => html`
     <style>
+        @import url('https://fonts.googleapis.com/css2?family=JetBrains+Mono&family=Roboto:wght@300&display=swap');
         body {
             --color-main-1: #F6757A;
             --color-main-2: #FEAE35;
@@ -9,6 +10,8 @@ flami(() => html`
             --color-contrast: #181425;
             background-color: var(--color-bg);
         }
+        * { font-family: 'JetBrains Mono', monospace; }
+        p { font-family: 'Roboto', sans-serif; }
     </style>
-    ${navbar(navItem('home', 'home-outline'), navItem('apps', 'apps-outline'))}
+    ${navbar(navItem('Home', 'home-outline'), navItem('Apps', 'apps-outline'))}
 `)
