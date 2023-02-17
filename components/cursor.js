c6c30a1a068629ad2bfa79cf81e8d2450bd5b927
JoelVerm <joel.vermeulen5@outlook.com>
2023-02-17 21:35:03 +0100

    made blob

--- components/cursor.js
+++ components/cursor.js
@@ -1,3 +1,41 @@
 export const page = () => html`
-    <style></style>
+    <style>
+        ${`
+        .cursor {
+            background: var(--color-main-1);
+            height: 200px;
+            width: 200px;
+            position: fixed;
+            left: ${mouseX}px;
+            top: ${mouseY}px;
+            translate: -50% -50%;
+            transition: opacity 0.5s;
+            opacity: ${mouseX == 0 && mouseY == 0 ? 0 : 1};
+            animation: cursorBlob 2s linear infinite;
+        }
+        @keyframes cursorBlob {
+            0%, 100% {
+                border-radius: 45% 33% 32% 49% / 32% 48% 34% 50%;
+            }
+            25% {
+                border-radius: 35% 38% 45% 32% / 47% 32% 36% 46%;
+            }
+            50% {
+                border-radius: 50% 34% 32% 33%/ 36% 47% 49% 38%;
+            }
+            75% {
+                border-radius: 32% 42% 46% 41% / 45% 36% 40% 45%;
+            }
+        }
+    `}
+    </style>
+    <div class="cursor"></div>
 `
+
+let mouseX = 0
+let mouseY = 0
+document.body.addEventListener('mousemove', e => {
+    mouseX = e.clientX
+    mouseY = e.clientY
+    update()
+})
